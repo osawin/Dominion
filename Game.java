d7e7b63d7bbf609b90afe96b363dbadba7c6ed88
Oliver Sawin <osawin@gmail.com>
2018-12-20 13:11:05 -0500

    Large Update
    Moving the classes other than Game into the Game class. Implementing the main function of Game. Creating the Copper and Village examples of card classes. Expanding the play function of Player

--- Game.java
+++ Game.java
@@ -1,57 +1,204 @@
 public class Game{
+	player[] group;
+	int[] sizes;
+	Card[] available;
+
     public static void main(String[] args) {
-        
+    	int num_players = 1
+        this.group = new Player[num_players];
+        this.sizes = new int[16];
+        this.available = new Card[16];
+        int i = 0;
+        while (i < 10)
+        {
+        	sizes[i] = 10;
+        	i++;
+        }
+        int currentPlayer = 0;
+        while (this.notOver())
+        {
+        	this.group[currentPlayer].play();
+        	currentPlayer++;
+        	if (currentPlayer >= num_players)
+        	{
+        		currentPlayer = 0;
+        	}
+        }
     }
-}
-
-public class card {
-	public boolean isAction();
-	public int treasure();
-	public String name();
-	public void play(player person);
-}
-
-public class player {
-	private card[] hand;
-	private card[] deck;
-	private card[] discard;
-	private card[] 
-	public static void play()
-	{
-		int action = 1;
-		int gold = 0;
-		printHand();
-		String answer;
-		int index;
-		Scanner in = new Scanner(System.in);
-		while (action > 1)
+    
+    private boolean notOver()
+    {
+    	return true;
+    }
+    
+	public class Card {
+		public boolean isAction;
+		public int treasure;
+		public int points;
+		public String name;
+		public int cost;
+		public Card()
 		{
-			System.out.println("Would you like to play an action? Y/N");
-			answer = in.nextLine();
-			if (answer == "Y")
+			this.isAction = false;
+			this.treasure = 0;
+			this.points = 0;
+			this.name = "";
+			this.cost = 0;
+		}
+		public void play(Player person);
+	}	
+	
+	public class Copper extends Card{
+		public Copper()
+		{
+			super();
+			this.treasure = 1;
+			this.name = "Copper";
+		}
+	}
+
+	public class Village extends Card{
+		public Village()
+		{
+			super();
+			this.isAction = true;
+			this.name = "Village";
+			this.cost = 3;
+		}
+		
+		public void play(Player person)
+		{
+			person.draw();
+			person.actions += 2;
+		}
+	}
+	
+	public class Player {
+		private ArrayList<Card> hand;
+		private ArrayList<Card> deck;
+		private ArrayList<Card> discard;
+		public int actions;
+		public int gold;
+		public int buys;
+		
+		public Player()
+		{	
+			this.hand = new ArrayList(5);
+			this.deck = new ArrayList(10);
+			this.discard = new ArrayList(10);
+			this.discard.add(new Copper());
+			this.discard.add(new Copper());
+			this.discard.add(new Copper());
+			this.discard.add(new Copper());
+			this.discard.add(new Copper());
+			this.discard.add(new Copper());
+			this.discard.add(new Estate());
+			this.discard.add(new Estate());
+			this.discard.add(new Estate());
+			this.addDiscard();
+		}
+		
+		private void addDiscard()
+		{
+			Random shuffle = new Random();
+			while (!this.discard.isEmpty())
 			{
-				printHand();
-				System.out.println("Which action would you like to play? (Type index, 0-based)");
-				index = in.nextInt();
-				if (index >= 0 && index < hand.length)
+				int i = shuffle.newInt(this.discard.size())
 				{
-					hand[index].play(this);
-					action--;
+					this.deck.add(this.discard[i]);
+					this.discard.remove(i);
 				}
-				else
+			}
+		}
+		
+		public void draw()
+		{
+			if (this.deck.isEmpty())
+			{
+				if (!this.discard.isEmpty())
 				{
-					action = 0;
+					this.addDiscard();
 				}
 			}
 			else
 			{
-				action = 0;
+				this.hand.add(deck(0));
+				this.deck.remove(0);
 			}
 		}
-		index = 0;
-		while (index < hand.length)
+		
+		private void printHand()
 		{
-			gold += hand[index].treasure();
+			int i = 0;
+			while (i < this.hand.size())
+			{
+				System.Out.println(this.hand[i].name + ", ");
+				i++;
+			}
+		}
+		
+		public static void play()
+		{
+			this.actions = 1;
+			buys = 1;
+			this.gold = 0;
+			this.printHand();
+			String answer;
+			int index;
+			Scanner in = new Scanner(System.in);
+			while (actions > 1)
+			{
+				System.out.println("Would you like to play an action? Y/N");
+				answer = in.nextLine();
+				if (answer == "Y")
+				{
+					this.printHand();
+					System.out.println("Which action would you like to play? (Type index, 0-based)");
+					index = in.nextInt();
+					if (index >= 0 && index < this.hand.length)
+					{
+						this.hand[index].play(this);
+						actions--;
+					}
+					else
+					{
+						this.actions = 0;
+					}
+				}
+				else
+				{
+					this.action = 0;
+				}
+			}
+			index = 0;
+			while (index < this.hand.length)
+			{
+				this.gold += this.hand[index].treasure;
+			}
+			Game.this.printAvailable();
+			while (buys > 0)
+			{
+				System.out.println("Which card would you like to buy? (Type index, 0-based, -1 to not buy anything)");
+				index = in.nextInt();
+				if (index > -1 && index < 16)
+				{
+					if (Game.this.available[index].cost <= gold && Game.this.sizes[index]>0)
+					{
+						this.discard.add(Game.this.available[index]);
+						this.gold -= Game.this.available[index].cost;
+						buys--;
+					}
+					else
+					{
+						System.out.println("Not a valid buy, try again");
+					}
+				}
+				else
+				{
+					buys = 0;
+				}
+			}
 		}
 	}
 }
+
